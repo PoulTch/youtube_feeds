4f6897bff3abae9c3133fe6387e1929cc134d48e
PoulTch <pavelchar@gmail.com>
2026-07-16 12:10:00 +0300

    db: sync solid cache migration file name with server

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_08_124349) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_16_090624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -33,6 +33,11 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_08_124349) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["channel_id"], name: "index_playlists_on_channel_id"
+  end
+
+  create_table "solid_cache_tables", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
